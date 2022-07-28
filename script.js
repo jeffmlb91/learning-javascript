6b6e17109b8dd9a9fe20617adbfc5682da51190c
jeffmlb91 <jeffmlb91@gmail.com>
2022-07-28 00:22:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Testing if browser is working
+// let name = "Freddy";
+// alert("This" + " " + name);
+// let age = 30;
+// alert("Freddy is " + age);
+
+let firstNumber = 3;
+let secondNumber = 2.4;
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 5;
+let e = 6;
+let f = 8;
+
+console.log("addition:" + " " + firstNumber + secondNumber);
+console.log("divison:" + " " + firstNumber / secondNumber);
+console.log("multiplication: " + " " + firstNumber * secondNumber);
+console.log("Remainder of:" + " " + (firstNumber % secondNumber));
+console.log(
+  "First raised to the power of b:" + " " + firstNumber ** secondNumber
+);
+
+console.log("Remander of" + " " + (3 % 2));
+console.log();
+console.log();
+console.log();
